833c498e26e8cad1e3996469d763be3ed3ae595d
ghouston16 <ghouston16@hotmail.com>
2022-02-02 21:01:55 +0000

    list of test users rendered

---
+++ src/UserList.svelte
@@ -0,0 +1,26 @@
+<script>
+    export let userList;
+  </script>
+  
+  <h3 class="uk-heading-divider">
+    User List </h3>
+  <div class="uk-table uk-table-divider">
+    <table class="uk-table">
+      <thead>
+        <th>
+          Email
+        </th>
+        <th>
+            Password
+          </th>
+      </thead>
+      <tbody class="uk-text-left">
+        {#each userList as user}
+          <tr>
+            <td>{user.email}</td>
+            <td>{user.password}</td>
+          </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
