cb4231fafae8d58e939422eaacab9c569a807d3c
ghouston16 <ghouston16@hotmail.com>
2022-02-02 23:14:53 +0000

    routing added

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,17 @@
   import PoiList from "./components/PoiList.svelte"
   import Users from "./pages/Users.svelte";
   import Pois from "./pages/Pois.svelte";
-
+  import Navigator from "./components/Navigator.svelte";
+  import Router from "svelte-spa-router";
   setContext("PoiService", new PoiService("http://127.0.0.1:8000"));
+  let routes = {
+    "/users": Users,
+    "/pois": Pois
+  }
 </script>
 
 <div class="uk-container">
   <h1> POI Client Application </h1>
-  <Users/>
-  <Pois/>
+  <Navigator/>
+  <Router {routes} />
 </div>
---
+++ src/components/Navigator.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Title from "./Title.svelte";
+  </script>
+  
+  <div class="uk-flex uk-flex-center uk-flex-middle uk-text-center uk-grid-small" uk-grid>
+    <Title title="Pois" subTitle="Select Pois or Users" />
+    <div>
+      <ul class="uk-iconnav uk-margin-left uk-margin-right">
+        <a href="/#/users" class="uk-card uk-card-small uk-card-hover uk-padding-small">
+          <i class="fas fa-hand-holding-usd fa-3x" style="color:rgb(153, 196, 74)" title="Pois" uk-tooltip></i>
+        </a>
+        <a href="/#/pois" class="uk-card uk-card-small uk-card-hover uk-padding-small">
+          <i class="fas fa-clipboard-list fa-3x" style="color:rgb(63, 122, 139)" title="Users" uk-tooltip></i>
+        </a>
+      </ul>
+    </div>
+  </div>
