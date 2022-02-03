0f5a5cb3fc38aee58a5312d0846e85abe7feaf10
ghouston16 <ghouston16@hotmail.com>
2022-02-03 17:26:18 +0000

    bug fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,12 @@
 <script>
   import { setContext } from "svelte";
-  import {PoiService} from "./poi-service";
+  import { PoiService } from "./poi-service";
   import Users from "./pages/Users.svelte";
   import Pois from "./pages/Pois.svelte";
   import Navigator from "./components/Navigator.svelte";
   import Router from "svelte-spa-router";
-import Main from "./pages/Main.svelte";
-import Login from "./pages/Login.svelte";
+  import Main from "./pages/Main.svelte";
+  import Login from "./pages/Login.svelte";
 
   setContext("PoiService", new PoiService("http://127.0.0.1:8000"));
 
@@ -16,11 +16,12 @@ import Login from "./pages/Login.svelte";
     "/users": Users,
     "/pois": Pois,
     "/logout": Main
-  }
+  };
 </script>
 
 <div class="uk-container">
-  <h1> POI Client Application </h1>
-  <Navigator/>
+  <h1>POI Client Application</h1>
+  <Navigator />
   <Router {routes} />
 </div>
+
